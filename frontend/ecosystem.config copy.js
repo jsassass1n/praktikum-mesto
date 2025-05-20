e912c7c36b78276500c08f73891cd47e7c9399ef
Dmitriy.Cherniy <dmitriy.cherniy@elnk.com>
2025-05-20 19:02:51 +0400

    Frontend ecosystem.config added

---
+++ frontend/ecosystem.config copy.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY,
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'export PATH=$HOME/.nvm/versions/node/v16.20.2/bin:$PATH && cd backend && pwd && npm ci && npm run build',
+    },
+  },
+};
