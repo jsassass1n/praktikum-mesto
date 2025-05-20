075bca3d6966112194f68879120731cd457272d2
Dmitriy.Cherniy <dmitriy.cherniy@elnk.com>
2025-05-20 19:13:58 +0400

    Code refactoring

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,31 +1,20 @@
-require('dotenv').config({ path: '.env.deploy' });
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY,
 } = process.env;
 
 module.exports = {
-  apps: [{
-    name: 'frontend',
-    script: 'npm',
-    args: 'start',
-    cwd: './',
-  }],
-
   deploy: {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/jsassass1n/praktikum-mesto.git',
+      repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy': `
-        rm -rf * &&
-        scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH} &&
-        scp .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}
-      `,
-      'post-deploy': 'npm i && npm run build && pm2 reload ecosystem.config.js --only frontend',
-      'clone': true
+      'post-deploy': 'export PATH=$HOME/.nvm/versions/node/v16.20.2/bin:$PATH && cd frontend && pwd && npm ci && npm run build',
     },
   },
 };
